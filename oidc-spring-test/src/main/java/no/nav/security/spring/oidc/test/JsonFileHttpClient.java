b16d8f94f908fc72d404bce898d522fdf4e9a38a
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-21 11:14:15 +0100

    some hygiene factor (mutability and friends) (#1)

--- oidc-spring-test/src/main/java/no/nav/security/spring/oidc/test/JsonFileHttpClient.java
+++ oidc-spring-test/src/main/java/no/nav/security/spring/oidc/test/JsonFileHttpClient.java
@@ -1,4 +1,5 @@
 package no.nav.security.spring.oidc.test;
+
 /*
  * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
  * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
@@ -8,8 +9,6 @@ package no.nav.security.spring.oidc.test;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.util.IOUtils;
 
@@ -17,37 +16,43 @@ import no.nav.security.oidc.http.HttpClient;
 import no.nav.security.oidc.http.HttpHeaders;
 
 public class JsonFileHttpClient implements HttpClient {
-	
-	ObjectMapper json = new ObjectMapper();
-	
-	private String metadataFile;
-	private String jwksFile;
-	
-	public JsonFileHttpClient(String metadataFile, String jwksFile) {
-		this.metadataFile = metadataFile;
-		this.jwksFile = jwksFile;
-	}
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> T get(String uri, HttpHeaders headers, Class<T> clazz) {
-		try {
-			if(uri.contains("metadata")) {
-				return json.readValue(this.getClass().getResourceAsStream(metadataFile), clazz);	
-			} else if(uri.contains("jwks")){			
-				String s = IOUtils.readInputStreamToString(this.getClass().getResourceAsStream(jwksFile), Charset.forName("UTF-8"));
-				System.out.println("content in metadata: " + s);
-				return (T)s;
-			} else {
-				return null;
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private static final ObjectMapper JSON = new ObjectMapper();
 
-	@Override
-	public <T> T post(String uri, String body, HttpHeaders headers, Class<T> clazz) {
-		return null;
-	}
+    private final String metadataFile;
+    private final String jwksFile;
+
+    public JsonFileHttpClient(String metadataFile, String jwksFile) {
+        this.metadataFile = metadataFile;
+        this.jwksFile = jwksFile;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T get(String uri, HttpHeaders headers, Class<T> clazz) {
+        try {
+            if (uri.contains("metadata")) {
+                return JSON.readValue(this.getClass().getResourceAsStream(metadataFile), clazz);
+            }
+            if (uri.contains("jwks")) {
+                String s = IOUtils.readInputStreamToString(this.getClass().getResourceAsStream(jwksFile),
+                        Charset.forName("UTF-8"));
+                System.out.println("content in metadata: " + s);
+                return (T) s;
+            }
+            return null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> T post(String uri, String body, HttpHeaders headers, Class<T> clazz) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [metadataFile=" + metadataFile + ", jwksFile=" + jwksFile + "]";
+    }
 }
--- oidc-spring-test/src/main/java/no/nav/security/spring/oidc/test/TokenGeneratorController.java
+++ oidc-spring-test/src/main/java/no/nav/security/spring/oidc/test/TokenGeneratorController.java
@@ -2,7 +2,6 @@ package no.nav.security.spring.oidc.test;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Optional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,53 +21,55 @@ import no.nav.security.spring.oidc.validation.api.Unprotected;
 @RestController
 @RequestMapping("/local")
 public class TokenGeneratorController {
-	
-	@Unprotected
-	@GetMapping("/jwt")
-	public String issueToken(@RequestParam(value="subject", defaultValue = "12345678910") String subject){
-		return JwtTokenGenerator.createSignedJWT(subject).serialize();
-	}
-	
-	@Unprotected
-	@GetMapping("/jwtclaims")
-	public SignedJWT jwtClaims(@RequestParam(value="subject", defaultValue = "12345678910") String subject){
-		return JwtTokenGenerator.createSignedJWT(subject);
-	}
-	
-	@Unprotected
-	@GetMapping("/cookie")
-	public Cookie addCookie(@RequestParam(value="subject", defaultValue = "12345678910") String subject, 
-							@RequestParam(value="cookiename", defaultValue = "localhost-idtoken") String cookieName, 
-							@RequestParam(value="redirect", required=false) String redirect,  
-							HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
-		SignedJWT token = JwtTokenGenerator.createSignedJWT(subject);
-		Cookie cookie = new Cookie(cookieName, token.serialize());
-		cookie.setDomain("localhost");
-		cookie.setPath("/");
-		response.addCookie(cookie);
-		if(redirect != null){
-			response.sendRedirect(redirect);
-		}
-		return cookie;	
-	}
-	
-	@Unprotected
-	@GetMapping("/jwks")
-	public String jwks() throws IOException{
-			return IOUtils.readInputStreamToString(getClass().getResourceAsStream(JwkGenerator.DEFAULT_JWKSET_FILE), Charset.defaultCharset());
-	}
-	
-	@Unprotected
-	@GetMapping("/metadata")
-	public String metadata() throws IOException{
-			return IOUtils.readInputStreamToString(getClass().getResourceAsStream("/metadata.json"), Charset.defaultCharset());
-	}
-	
-	@Unprotected
-	@GetMapping("/jwksfull")
-	public JWKSet jwkSet(){
-		return JwkGenerator.getJWKSet();	
-	}
-	
+
+    @Unprotected
+    @GetMapping("/jwt")
+    public String issueToken(@RequestParam(value = "subject", defaultValue = "12345678910") String subject) {
+        return JwtTokenGenerator.createSignedJWT(subject).serialize();
+    }
+
+    @Unprotected
+    @GetMapping("/jwtclaims")
+    public SignedJWT jwtClaims(@RequestParam(value = "subject", defaultValue = "12345678910") String subject) {
+        return JwtTokenGenerator.createSignedJWT(subject);
+    }
+
+    @Unprotected
+    @GetMapping("/cookie")
+    public Cookie addCookie(@RequestParam(value = "subject", defaultValue = "12345678910") String subject,
+            @RequestParam(value = "cookiename", defaultValue = "localhost-idtoken") String cookieName,
+            @RequestParam(value = "redirect", required = false) String redirect,
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        SignedJWT token = JwtTokenGenerator.createSignedJWT(subject);
+        Cookie cookie = new Cookie(cookieName, token.serialize());
+        cookie.setDomain("localhost");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        if (redirect != null) {
+            response.sendRedirect(redirect);
+        }
+        return cookie;
+    }
+
+    @Unprotected
+    @GetMapping("/jwks")
+    public String jwks() throws IOException {
+        return IOUtils.readInputStreamToString(getClass().getResourceAsStream(JwkGenerator.DEFAULT_JWKSET_FILE),
+                Charset.defaultCharset());
+    }
+
+    @Unprotected
+    @GetMapping("/metadata")
+    public String metadata() throws IOException {
+        return IOUtils.readInputStreamToString(getClass().getResourceAsStream("/metadata.json"),
+                Charset.defaultCharset());
+    }
+
+    @Unprotected
+    @GetMapping("/jwksfull")
+    public JWKSet jwkSet() {
+        return JwkGenerator.getJWKSet();
+    }
+
 }
