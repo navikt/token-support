34826541959a78712c86abb8cca8d1120066d06c
Eirik <eiriksletteberg@gmail.com>
2018-05-04 09:19:17 +0200

    change input path of encrypted gpg key

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_5569a6fe8f7a_key -iv $encrypted_5569a6fe8f7a_iv \
-  -in codesigning.asc.enc -out travis/codesigning.asc -d
+  -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
     mvn --settings travis/settings.xml deploy -Prelease -DskipTests=true
 fi
