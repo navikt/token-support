3b283f803dbcec88efb01978e46d436fa51958e4
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2018-08-08 09:05:15 +0200

    fix imports

--- oidc-support/src/main/java/no/nav/security/oidc/context/OIDCClaims.java
+++ oidc-support/src/main/java/no/nav/security/oidc/context/OIDCClaims.java
@@ -6,7 +6,6 @@ package no.nav.security.oidc.context;
  * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
  */
 import java.text.ParseException;
-import java.util.Arrays;
 import java.util.List;
 
 import com.nimbusds.jwt.JWT;
