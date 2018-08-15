ce7aea1fa1060a742dfa9789ccb96c3246d94a25
Nordstoga, Aasmund <Aasmund.Nordstoga@nav.no>
2018-08-15 14:54:02 +0200

    Ønsker å bruke to issuers, ein på internett som må bruke proxy, og ein intern som ikkje kan bruke proxy. La til kode som alltid unngår å bruke proxy dersom discovery-url inneholder adeo.no.
    Ideelt sett kunne ein kanskje knytta proxy eller ei til kvar enkelt issuer.

--- oidc-support/src/main/java/no/nav/security/oidc/OIDCConstants.java
+++ oidc-support/src/main/java/no/nav/security/oidc/OIDCConstants.java
@@ -10,6 +10,7 @@ public class OIDCConstants {
 	public static final String COOKIE_NAME = "%s-idtoken";
 	public static final String AUTHORIZATION_HEADER = "Authorization";
 	public static final String OIDC_VALIDATION_CONTEXT = "no.nav.security.oidc.validation.context";
+	public static final String NO_PROXY_HOSTNAME = "adeo.no";
 	
 	public static String getDefaultCookieName(String issuer) {
 		return String.format(COOKIE_NAME, issuer);
