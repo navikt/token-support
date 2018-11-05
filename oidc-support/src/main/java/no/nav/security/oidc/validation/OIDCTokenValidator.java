e08d397f70517415365991184cc20eb6e84c6ae9
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-05 16:54:20 +0100

    add expiration date to OIDCTokenVaidationException and log this

--- oidc-support/src/main/java/no/nav/security/oidc/validation/OIDCTokenValidator.java
+++ oidc-support/src/main/java/no/nav/security/oidc/validation/OIDCTokenValidator.java
@@ -10,6 +10,7 @@ package no.nav.security.oidc.validation;
 import java.net.URL;
 import java.text.ParseException;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,61 +26,74 @@ import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.openid.connect.sdk.Nonce;
 import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator;
+
 import no.nav.security.oidc.exceptions.OIDCTokenValidatorException;
 
-
 public class OIDCTokenValidator {
-	private static Logger log = LoggerFactory.getLogger(OIDCTokenValidator.class);
-	private final JWSAlgorithm jwsAlg = JWSAlgorithm.RS256;
-	private final Map<String, IDTokenValidator> audienceValidatorMap;
+    private static Logger log = LoggerFactory.getLogger(OIDCTokenValidator.class);
+    private final JWSAlgorithm jwsAlg = JWSAlgorithm.RS256;
+    private final Map<String, IDTokenValidator> audienceValidatorMap;
 
-	public OIDCTokenValidator(String issuer, String clientId, URL jwkSetUrl, ResourceRetriever jwksResourceRetriever) {		
-		this(issuer, Collections.singletonList(clientId), jwkSetUrl, jwksResourceRetriever);
-	}
-	
-	public OIDCTokenValidator(String issuer, List<String> acceptedAudience, URL jwkSetUrl, ResourceRetriever jwksResourceRetriever) {
-		this.audienceValidatorMap = initializeMap(issuer, acceptedAudience, jwkSetUrl, jwksResourceRetriever);
-	}
+    public OIDCTokenValidator(String issuer, String clientId, URL jwkSetUrl, ResourceRetriever jwksResourceRetriever) {
+        this(issuer, Collections.singletonList(clientId), jwkSetUrl, jwksResourceRetriever);
+    }
 
-	public void assertValidToken(String tokenString) throws OIDCTokenValidatorException {
-		assertValidToken(tokenString, null);
-	}
+    public OIDCTokenValidator(String issuer, List<String> acceptedAudience, URL jwkSetUrl,
+            ResourceRetriever jwksResourceRetriever) {
+        this.audienceValidatorMap = initializeMap(issuer, acceptedAudience, jwkSetUrl, jwksResourceRetriever);
+    }
 
-	public void assertValidToken(String tokenString, String expectedNonce) throws OIDCTokenValidatorException {
-		try {
-			JWT token = JWTParser.parse(tokenString);	
-			get(token).validate(token, expectedNonce != null ? new Nonce(expectedNonce) : null);
-		} catch (Throwable t) {
-			throw new OIDCTokenValidatorException("token validation failed: " + t, t);
-		}
-	}
-	
-	protected IDTokenValidator get(JWT jwt) throws ParseException, OIDCTokenValidatorException {
-		List<String> tokenAud = jwt.getJWTClaimsSet().getAudience();
-		for (String aud : tokenAud) {
-			if(audienceValidatorMap.containsKey(aud)){
-				return audienceValidatorMap.get(aud);
-			}
-		}
-		log.warn("could not find validator for token audience:" + tokenAud);
-		throw new OIDCTokenValidatorException("could not find appropriate validator to validate token. check your config.");
-	}
-	
-	protected IDTokenValidator createValidator(String issuer, String clientId, URL jwkSetUrl, ResourceRetriever jwksResourceRetriever){
-		Issuer iss = new Issuer(issuer);
-		ClientID clientID = new ClientID(clientId);
-		IDTokenValidator validator = new IDTokenValidator(iss, clientID, jwsAlg, jwkSetUrl, jwksResourceRetriever);
-		return validator;
-	}
-	
-	private Map<String, IDTokenValidator> initializeMap(String issuer, List<String> acceptedAudience, URL jwkSetUrl, ResourceRetriever jwksResourceRetriever){
-		if(acceptedAudience == null || acceptedAudience.isEmpty()){
-			throw new IllegalArgumentException("accepted audience cannot be null or empty in validator config.");
-		}
-		Map<String, IDTokenValidator> map = new HashMap<>();
-		for (String aud : acceptedAudience) {
-			map.put(aud, createValidator(issuer, aud, jwkSetUrl, jwksResourceRetriever));
-		}
-		return map;
-	}	
+    public void assertValidToken(String tokenString) throws OIDCTokenValidatorException {
+        assertValidToken(tokenString, null);
+    }
+
+    public void assertValidToken(String tokenString, String expectedNonce) throws OIDCTokenValidatorException {
+        JWT token = null;
+        try {
+            token = JWTParser.parse(tokenString);
+            get(token).validate(token, expectedNonce != null ? new Nonce(expectedNonce) : null);
+        } catch (Throwable t) {
+            throw new OIDCTokenValidatorException("Token validation failed", expiryDate(token), t);
+        }
+    }
+
+    private static Date expiryDate(JWT token) {
+        try {
+            return token != null ? token.getJWTClaimsSet().getExpirationTime() : null;
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    protected IDTokenValidator get(JWT jwt) throws ParseException, OIDCTokenValidatorException {
+        List<String> tokenAud = jwt.getJWTClaimsSet().getAudience();
+        for (String aud : tokenAud) {
+            if (audienceValidatorMap.containsKey(aud)) {
+                return audienceValidatorMap.get(aud);
+            }
+        }
+        log.warn("could not find validator for token audience:" + tokenAud);
+        throw new OIDCTokenValidatorException(
+                "could not find appropriate validator to validate token. check your config.");
+    }
+
+    protected IDTokenValidator createValidator(String issuer, String clientId, URL jwkSetUrl,
+            ResourceRetriever jwksResourceRetriever) {
+        Issuer iss = new Issuer(issuer);
+        ClientID clientID = new ClientID(clientId);
+        IDTokenValidator validator = new IDTokenValidator(iss, clientID, jwsAlg, jwkSetUrl, jwksResourceRetriever);
+        return validator;
+    }
+
+    private Map<String, IDTokenValidator> initializeMap(String issuer, List<String> acceptedAudience, URL jwkSetUrl,
+            ResourceRetriever jwksResourceRetriever) {
+        if (acceptedAudience == null || acceptedAudience.isEmpty()) {
+            throw new IllegalArgumentException("accepted audience cannot be null or empty in validator config.");
+        }
+        Map<String, IDTokenValidator> map = new HashMap<>();
+        for (String aud : acceptedAudience) {
+            map.put(aud, createValidator(issuer, aud, jwkSetUrl, jwksResourceRetriever));
+        }
+        return map;
+    }
 }
