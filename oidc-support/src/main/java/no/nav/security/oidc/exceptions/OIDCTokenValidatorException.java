4ddd0e0333e73aa40ae9ef78efc2576f7d576f64
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-06 19:43:58 +0100

    add expiration date to OIDCTokenVaidationException and log this (#17)

--- oidc-support/src/main/java/no/nav/security/oidc/exceptions/OIDCTokenValidatorException.java
+++ oidc-support/src/main/java/no/nav/security/oidc/exceptions/OIDCTokenValidatorException.java
@@ -1,20 +1,26 @@
 package no.nav.security.oidc.exceptions;
 
-/*
- * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
- * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
- * PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
- */
+import java.util.Date;
 
 @SuppressWarnings("serial")
 public class OIDCTokenValidatorException extends Exception {
-	
-	public OIDCTokenValidatorException(String message){
-		super(message);
-	}
-	
-	public OIDCTokenValidatorException(String message, Throwable cause){
-		super(message,  cause);
-	}
+
+    private final Date expiryDate;
+
+    public OIDCTokenValidatorException(String message) {
+        this(message, null, null);
+    }
+
+    public OIDCTokenValidatorException(String message, Date expiryDate) {
+        this(message, expiryDate, null);
+    }
+
+    public OIDCTokenValidatorException(String message, Date expiryDate, Throwable cause) {
+        super(message, cause);
+        this.expiryDate = expiryDate;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
 }
--- oidc-support/src/main/java/no/nav/security/oidc/filter/OIDCTokenValidationFilter.java
+++ oidc-support/src/main/java/no/nav/security/oidc/filter/OIDCTokenValidationFilter.java
@@ -1,4 +1,5 @@
 package no.nav.security.oidc.filter;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -19,12 +20,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import no.nav.security.oidc.configuration.MultiIssuerConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.nimbusds.jwt.JWTParser;
 
+import no.nav.security.oidc.configuration.MultiIssuerConfiguration;
 import no.nav.security.oidc.context.OIDCClaims;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.security.oidc.context.OIDCValidationContext;
@@ -33,69 +34,69 @@ import no.nav.security.oidc.exceptions.OIDCTokenValidatorException;
 
 public class OIDCTokenValidationFilter implements Filter {
 
-	private Logger logger = LoggerFactory.getLogger(OIDCTokenValidationFilter.class);
-	private final MultiIssuerConfiguration config;
-	private final OIDCRequestContextHolder contextHolder;
-	
-	public OIDCTokenValidationFilter(MultiIssuerConfiguration oidcConfig, OIDCRequestContextHolder contextHolder) {
-		this.config = oidcConfig;
-		this.contextHolder = contextHolder;
-	}
+    private Logger logger = LoggerFactory.getLogger(OIDCTokenValidationFilter.class);
+    private final MultiIssuerConfiguration config;
+    private final OIDCRequestContextHolder contextHolder;
 
-	@Override
-	public void destroy() {
-		
-	}
+    public OIDCTokenValidationFilter(MultiIssuerConfiguration oidcConfig, OIDCRequestContextHolder contextHolder) {
+        this.config = oidcConfig;
+        this.contextHolder = contextHolder;
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		if (request instanceof HttpServletRequest) {
-			doTokenValidation((HttpServletRequest) request, (HttpServletResponse) response, chain);
-		} else {
-			chain.doFilter(request, response);
-		}
-	}
+    @Override
+    public void destroy() {
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
-	}
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            doTokenValidation((HttpServletRequest) request, (HttpServletResponse) response, chain);
+        }
+        else {
+            chain.doFilter(request, response);
+        }
+    }
 
-	private void doTokenValidation(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		
-		List<TokenContext> tokensOnRequest = TokenRetriever.retrieveTokens(config, request);
-		List<TokenContext> validatedTokens = new ArrayList<>();
-		for(TokenContext token : tokensOnRequest) {
-			long start = System.currentTimeMillis();
-			try {
-				
-				config.getIssuer(token.getIssuer()).getTokenValidator().
-					assertValidToken(token.getIdToken());
-				validatedTokens.add(token);
-				logger.debug("token " + token.getIssuer() + " validated OK");
-			} catch(OIDCTokenValidatorException ve) {
-				logger.warn("invalid token for issuer [" + token.getIssuer() + "].", ve);
-			}
-			long stop = System.currentTimeMillis();
-			logger.debug("validated token [" + token.getIssuer() + "] in " + (stop-start) + "ms");
-		}
-		OIDCValidationContext validationContext = new OIDCValidationContext();
-		for(TokenContext validatedToken : validatedTokens) {
-			try {
-				validationContext.addValidatedToken(validatedToken.getIssuer(), validatedToken, 
-						new OIDCClaims(JWTParser.parse(validatedToken.getIdToken())));
-			} catch (ParseException e) {
-				logger.warn("failed to parse token despite validated: " + e, e);
-			}
-		}
-		contextHolder.setOIDCValidationContext(validationContext);
-		try {
-			chain.doFilter(request, response);
-		} finally {
-			contextHolder.setOIDCValidationContext(null);
-		}
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    private void doTokenValidation(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        List<TokenContext> tokensOnRequest = TokenRetriever.retrieveTokens(config, request);
+        List<TokenContext> validatedTokens = new ArrayList<>();
+        for (TokenContext token : tokensOnRequest) {
+            long start = System.currentTimeMillis();
+            try {
+
+                config.getIssuer(token.getIssuer()).getTokenValidator().assertValidToken(token.getIdToken());
+                validatedTokens.add(token);
+                logger.debug("token " + token.getIssuer() + " validated OK");
+            } catch (OIDCTokenValidatorException ve) {
+                logger.warn("Invalid token for issuer [{}, expires at {}]", token.getIssuer(), ve.getExpiryDate(), ve);
+            }
+            long stop = System.currentTimeMillis();
+            logger.debug("validated token [" + token.getIssuer() + "] in " + (stop - start) + "ms");
+        }
+        OIDCValidationContext validationContext = new OIDCValidationContext();
+        for (TokenContext validatedToken : validatedTokens) {
+            try {
+                validationContext.addValidatedToken(validatedToken.getIssuer(), validatedToken,
+                        new OIDCClaims(JWTParser.parse(validatedToken.getIdToken())));
+            } catch (ParseException e) {
+                logger.warn("failed to parse token despite validated: " + e, e);
+            }
+        }
+        contextHolder.setOIDCValidationContext(validationContext);
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            contextHolder.setOIDCValidationContext(null);
+        }
+    }
 
 }
