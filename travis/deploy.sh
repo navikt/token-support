86e17f635d9b4d604f5f0f864338bc2507aaa906
Tommy Trøen <tommytroen@gmail.com>
2019-10-29 15:22:57 +0100

    move pipeline to circleci (#70)
    * GPG 2.1 requires --pinentry-mode to be set to loopback in order to pick up gpg.passphrase value defined in Maven settings.xml.

--- travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_91adec119052_key -iv $encrypted_91adec119052_iv \
-  -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-    gpg --fast-import travis/codesigning.asc
-    mvn --settings travis/settings.xml deploy -Prelease,deploy-to-sonatype -DskipTests=true
-fi
