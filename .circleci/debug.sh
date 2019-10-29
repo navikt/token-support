d543a57070b7bc0a2eb044023e0e60cc98185fbe
tommytroen <tommytroen@gmail.com>
2019-10-29 12:11:51 +0100

    add settings.xml and clean up commands
    add export GPG_TTY=$(tty) to command to avoid "gpg: signing failed: Inappropriate ioctl for device"
    
    add export GPG_TTY=$(tty) to command to avoid "gpg: signing failed: Inappropriate ioctl for device"
    
    update sonatyper user and pwd from context
    
    add -Xe switch for debug
    
    add -Xe switch for debug
    
    remove build step for debug
    
    degrade docker image to java 8
    
    add env debug for now
    
    add env debug for now
    
    print out gpg version
    
    remove tty env stuff
    
    print tty stuff
    
    downgrade docker again
    
    bump back to 11

---
+++ .circleci/debug.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ -z "${GPG_KEY_BASE64}" ];
+then
+  echo "not GPG_KEY_BASE64";
+else
+  echo "yupp GPG_KEY_BASE64";
+fi
+
+if [ -z "${GPG_KEY_NAME}" ];
+then
+  echo "not GPG_KEY_NAME";
+else
+  echo "yupp GPG_KEY_NAME";
+fi
+
+if [ -z "${GPG_PASSPHRASE}" ];
+then
+  echo "not GPG_PASSPHRASE";
+else
+  echo "yupp GPG_PASSPHRASE";
+fi
+
+gpg --version
+echo "tty stuff: $(tty)"
