6689893933b7c7634e0a0ea904f7298e7b4bfa67
tommytroen <tommytroen@gmail.com>
2020-01-16 11:40:10 +0100

    refactor(token-client-core) add tests

---
+++ token-client-spring/src/test/java/no/nav/security/token/support/client/spring/oauth2/ClientConfigurationPropertiesTestWithResourceUrl.java
@@ -0,0 +1,54 @@
+package no.nav.security.token.support.client.spring.oauth2;
+
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
+import no.nav.security.token.support.client.core.ClientAuthenticationProperties;
+import no.nav.security.token.support.client.core.ClientProperties;
+import no.nav.security.token.support.client.core.oauth2.OnBehalfOfGrantRequest;
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties;
+import no.nav.security.token.support.core.context.TokenValidationContextHolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {OAuth2ClientConfiguration.class, RestTemplateAutoConfiguration.class})
+@ActiveProfiles("test-withresourceurl")
+class ClientConfigurationPropertiesTestWithResourceUrl {
+
+    @MockBean
+    private TokenValidationContextHolder tokenValidationContextHolder;
+
+    @Autowired
+    private ClientConfigurationProperties clientConfigurationProperties;
+
+    @BeforeEach
+    void before(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testClientConfigIsValid() {
+        assertThat(clientConfigurationProperties).isNotNull();
+        assertThat(clientConfigurationProperties.getRegistration()).isNotNull();
+        ClientProperties clientProperties =
+            clientConfigurationProperties.getRegistration().values().stream().findFirst().orElse(null);
+        assertThat(clientProperties).isNotNull();
+        ClientAuthenticationProperties auth = clientProperties.getAuthentication();
+        assertThat(auth).isNotNull();
+        assertThat(auth.getClientAuthMethod()).isNotNull();
+        assertThat(auth.getClientId()).isNotNull();
+        assertThat(auth.getClientSecret()).isNotNull();
+        assertThat(clientProperties.getScope()).isNotEmpty();
+        assertThat(clientProperties.getTokenEndpointUrl()).isNotNull();
+        assertThat(clientProperties.getGrantType().getValue()).isNotNull();
+        assertThat(clientProperties.getResourceUrl()).isNotNull();
+    }
+}
