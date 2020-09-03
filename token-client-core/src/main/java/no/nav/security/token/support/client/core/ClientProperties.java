cfb2d3b2affa870fb2ff24e39f4041ee21fed9f9
Tommy Trøen <tommy.troen@nav.no>
2020-09-03 10:15:37 +0200

    feat(token-client-*): support well-known url if token endpoint url is not set (#205)
    * add new property `well-known-url:` to `no.nav.security.jwt.client.registration.[somename]`
    * will populate `token-endpoint-url` with value retrieved from metadata

--- token-client-core/src/main/java/no/nav/security/token/support/client/core/ClientProperties.java
+++ token-client-core/src/main/java/no/nav/security/token/support/client/core/ClientProperties.java
@@ -1,8 +1,13 @@
 package no.nav.security.token.support.client.core;
 
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.oauth2.sdk.as.AuthorizationServerMetadata;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
@@ -13,7 +18,7 @@ import java.util.function.Supplier;
 @Getter
 @ToString
 @EqualsAndHashCode
-@Builder(toBuilder = true)
+
 public class ClientProperties {
 
     private static final List<OAuth2GrantType> GRANT_TYPES = List.of(
@@ -31,20 +36,46 @@ public class ClientProperties {
     private final ClientAuthenticationProperties authentication;
     private final URI resourceUrl;
     private final TokenExchangeProperties tokenExchange;
+    private final URI wellKnownUrl;
+    private AuthorizationServerMetadata authorizationServerMetadata;
+    private ResourceRetriever resourceRetriever;
 
-    public ClientProperties(@NotNull URI tokenEndpointUrl,
+    @Builder(toBuilder = true)
+    public ClientProperties(URI tokenEndpointUrl,
+                            URI wellKnownUrl,
                             @NotNull OAuth2GrantType grantType,
                             List<String> scope,
                             @NotNull ClientAuthenticationProperties authentication,
                             URI resourceUrl,
                             TokenExchangeProperties tokenExchange
     ) {
-        this.tokenEndpointUrl = tokenEndpointUrl;
+        this.wellKnownUrl = wellKnownUrl;
+
+        if(tokenEndpointUrl != null){
+            this.tokenEndpointUrl = tokenEndpointUrl;
+        } else {
+            this.resourceRetriever = new DefaultResourceRetriever();
+            this.authorizationServerMetadata = retrieveAuthorizationServerMetadata();
+            this.tokenEndpointUrl = this.authorizationServerMetadata.getTokenEndpointURI();
+        }
         this.grantType = getSupported(grantType);
         this.scope = Optional.ofNullable(scope).orElse(Collections.emptyList());
         this.authentication = authentication;
         this.resourceUrl = resourceUrl;
         this.tokenExchange = tokenExchange;
+    }
+
+    private AuthorizationServerMetadata retrieveAuthorizationServerMetadata(){
+        if (wellKnownUrl == null) {
+            throw new OAuth2ClientException("wellKnownUrl cannot be null, please check your configuration.");
+        }
+        try {
+            return AuthorizationServerMetadata.parse(
+                resourceRetriever.retrieveResource(wellKnownUrl.toURL()).getContent()
+            );
+        } catch (ParseException | IOException e) {
+            throw new OAuth2ClientException("received exception when retrieving metadata from url " + wellKnownUrl, e);
+        }
     }
 
     private static OAuth2GrantType getSupported(OAuth2GrantType oAuth2GrantType) {
