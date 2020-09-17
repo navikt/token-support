5d68d7a1ea73a0d2fe25d2ac780e1fdf255a7329
yousher <youssef.bel.mekki@nav.no>
2020-09-17 13:55:03 +0200

    fix: remove stacktrace from info log about invalid token for an issuer

--- token-validation-core/src/main/java/no/nav/security/token/support/core/validation/JwtTokenValidationHandler.java
+++ token-validation-core/src/main/java/no/nav/security/token/support/core/validation/JwtTokenValidationHandler.java
@@ -62,7 +62,7 @@ public class JwtTokenValidationHandler {
             return Optional.empty();
 
         } catch (JwtTokenValidatorException e) {
-            LOG.info("found invalid token for issuer [{}, expires at {}]", jwtToken.getIssuer(), e.getExpiryDate(), e);
+            LOG.info("found invalid token for issuer [{}, expires at {}]", jwtToken.getIssuer(), e.getExpiryDate());
             return Optional.empty();
         }
     }
