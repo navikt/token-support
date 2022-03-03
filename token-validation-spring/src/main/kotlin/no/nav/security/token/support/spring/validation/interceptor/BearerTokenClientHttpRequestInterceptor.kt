f142346871b08482a4ebfa25bb62a32b049da57c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-03 11:38:52 +0100

    https://github.com/navikt/token-support/issues/423

--- token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/validation/interceptor/BearerTokenClientHttpRequestInterceptor.kt
+++ token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/validation/interceptor/BearerTokenClientHttpRequestInterceptor.kt
@@ -9,17 +9,27 @@ import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.http.client.ClientHttpResponse
 import java.io.IOException
 
-class BearerTokenClientHttpRequestInterceptor(private val holder: TokenValidationContextHolder) : ClientHttpRequestInterceptor {
+class BearerTokenClientHttpRequestInterceptor(private val holder: TokenValidationContextHolder,
+                                              private val enabled: Boolean = true) : ClientHttpRequestInterceptor {
     private val log = LoggerFactory.getLogger(BearerTokenClientHttpRequestInterceptor::class.java)
 
     @Throws(IOException::class)
-    override fun intercept(req: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
-        holder.tokenValidationContext?.apply {
-            if (hasValidToken()) {
-                log.debug("Adding tokens to Authorization header")
-                req.headers.add(AUTHORIZATION_HEADER, issuers.joinToString { "Bearer " + getJwtToken(it).tokenAsString })
-            }
-        } ?: log.debug("no tokens found, nothing added to request")
+    override fun intercept(req: HttpRequest,
+                           body: ByteArray,
+                           execution: ClientHttpRequestExecution): ClientHttpResponse {
+        if (enabled) {
+            holder.tokenValidationContext?.apply {
+                if (hasValidToken()) {
+                    log.debug("Adding tokens to Authorization header")
+                    req.headers.add(
+                            AUTHORIZATION_HEADER,
+                            issuers.joinToString { "Bearer " + getJwtToken(it).tokenAsString })
+                }
+            } ?: log.debug("no tokens found, nothing added to request")
+        }
+        else {
+            log.trace("is disabled, no bearer propagated, make sure it is propagated by another means")
+        }
         return execution.execute(req, body)
     }
 }
