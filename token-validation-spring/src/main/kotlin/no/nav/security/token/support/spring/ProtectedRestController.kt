98adc4d904b4702424ca634f3fdabdc8d1b8da99
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-12 23:06:57 +0200

    unprotected

--- token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/ProtectedRestController.kt
+++ token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/ProtectedRestController.kt
@@ -1,6 +1,7 @@
 package no.nav.security.token.support.spring
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.core.annotation.AliasFor
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,3 +20,12 @@ annotation class ProtectedRestController(@get: AliasFor(annotation = ProtectedWi
                                          @get: AliasFor(annotation = ProtectedWithClaims::class, attribute = "claimMap") val claimMap: Array<String> = ["acr=Level4"],
                                          @get: AliasFor(annotation = RequestMapping::class, attribute = "value") val value:  Array<String> = ["/"],
                                          @get: AliasFor(annotation = RequestMapping::class, attribute = "produces") val produces: Array<String> = [APPLICATION_JSON_VALUE])
+
+@RestController
+@MustBeDocumented
+@Unprotected
+@Target(ANNOTATION_CLASS, CLASS)
+@Retention(RUNTIME)
+@RequestMapping
+annotation class UnprotectedRestController(@get: AliasFor(annotation = RequestMapping::class, attribute = "value") val value:  Array<String> = ["/"],
+                                           @get: AliasFor(annotation = RequestMapping::class, attribute = "produces") val produces: Array<String> = [APPLICATION_JSON_VALUE])
