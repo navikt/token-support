eced7db33553fbd32db00890941e113d031dc6ab
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-08 15:03:33 +0100

    set default value for issuer map

--- token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/MultiIssuerProperties.kt
+++ token-validation-spring/src/main/kotlin/no/nav/security/token/support/spring/MultiIssuerProperties.kt
@@ -4,10 +4,9 @@ import jakarta.validation.Valid
 import no.nav.security.token.support.core.configuration.IssuerProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
 
 @ConfigurationProperties("no.nav.security.jwt")
 @EnableConfigurationProperties
 @Validated
-data class MultiIssuerProperties(@Valid val issuer: Map<String,IssuerProperties>)
+data class MultiIssuerProperties(@Valid val issuer: Map<String,IssuerProperties> = emptyMap())
