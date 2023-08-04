93c78655efc7212c0e0012f2de393876aa7d741a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-08-04 08:46:11 +0200

    sonarcloud

--- token-validation-core/src/main/java/no/nav/security/token/support/core/utils/EnvUtil.java
+++ token-validation-core/src/main/java/no/nav/security/token/support/core/utils/EnvUtil.java
@@ -1,27 +1,30 @@
 package no.nav.security.token.support.core.utils;
 
-public class EnvUtil {
+public
+class EnvUtil {
 
-    private EnvUtil()  {
+    private
+    EnvUtil() {
 
     }
-     static  String  FSS = "fss";
-     static  String SBS = "sbs";
-     static  String LOCAL = "local";
-     static  String  GCP = "gcp";
-     static  String  TEST = "test";
-     static  String  DEV = "dev";
-     static  String PROD = "prod";
-    static  String DEV_GCP = join(DEV, GCP);
-    static  String PROD_GCP = join(PROD, GCP);
-    static  String PROD_SBS = join(PROD, SBS);
-    static  String DEV_SBS = join(DEV, SBS);
-    static  String PROD_FSS = join(PROD, FSS);
-    static  String DEV_FSS = join(DEV, FSS);
-    static String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
+
+    static final String FSS = "fss";
+    static final String SBS = "sbs";
+    static final String LOCAL = "local";
+    static final String GCP = "gcp";
+    static final String TEST = "test";
+    static final String DEV = "dev";
+    static final String PROD = "prod";
+    static final String DEV_GCP = join(DEV, GCP);
+    static final String PROD_GCP = join(PROD, GCP);
+    static final String PROD_SBS = join(PROD, SBS);
+    static final String DEV_SBS = join(DEV, SBS);
+    static final String PROD_FSS = join(PROD, FSS);
+    static final String DEV_FSS = join(DEV, FSS);
+    static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
+
     private static String join(String env, String cluster) {
         return env + "-" + cluster;
     }
-
 
 }
