15403aac79b89c4e4a5c3cdbe376b413ca02d9ce
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2023-08-24 18:42:04 +0200

    feat(token-validation-core): add new default configurable jwt validator
    This validator is meant to replace the two existing validator
    implementations: ConfigurableJwtTokenValidator and
    DefaultJwtTokenValidator.
    
    There are multiple issues with both of these validators.
    
    For ConfigurableJwtTokenValidator:
    
    - audience is never validated, regardless of it being defined as an
      optional claim or not.
    - not-before is set as required by default. Many providers in the
      wild do not include this claim (e.g. ID-porten). We should still
      validate the claim if present, but we shouldn't require the existence
      of the claim.
    - the issued at claim is not validated, beyond existing in the JWT.
    
    For DefaultJwtTokenValidator:
    
    - Effectively requires the JWT to conform to an id_token in the OIDC
      Spec, which means that claims such as audience is now required.
    - The Nimbus IDTokenValidator only allows the client to accept a single
      audience, which is its client_id. This is in accordance with the OIDC
      spec, but we don't really pass id_tokens to resource servers
      (anymore) - which is what this library is primarily meant for.
    - The previous restriction means that we need to maintain a separate
      backing validator for each accepted audience. In most cases we
      want to accept a token if there is at least one matching accepted audience
      (in the case of multiple values in the audience claim).
    - The validator will also look for and require the existence of the azp
      claim in the case of a JWT containing more than one audience, which
      again is only defined in the OIDC specifications for validating id_tokens

---
+++ token-validation-core/src/main/java/no/nav/security/token/support/core/validation/DefaultConfigurableJwtValidator.java
@@ -0,0 +1,115 @@
+package no.nav.security.token.support.core.validation;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimNames;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import no.nav.security.token.support.core.exceptions.JwtTokenValidatorException;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * The default configurable JwtTokenValidator.
+ * Configures sane defaults and delegates verification to {@link DefaultJwtClaimsVerifier}:
+ *
+ * <p>The following set of claims are required by default and <i>must</i>be present in the JWTs:</p>
+ * <ul>
+ *     <li>iss - Issuer</li>
+ *     <li>sub - Subject</li>
+ *     <li>aud - Audience</li>
+ *     <li>exp - Expiration Time</li>
+ *     <li>iat - Issued At</li>
+ * </ul>
+ *
+ * <p>Otherwise, the following checks are in place:</p>
+ * <ul>
+ *     <li>The issuer ("iss") claim value must match exactly with the specified accepted issuer value.</li>
+ *     <li><i>At least one</i> of the values in audience ("aud") claim must match one of the specified accepted audiences.</li>
+ *     <li>Time validity checks are performed on the issued at ("iat"), expiration ("exp") and not-before ("nbf") claims if and only if they are present.</li>
+ * </ul>
+ *
+ * <p>Note: the not-before ("nbf") claim is <i>not</i> a required claim. Conversely, the expiration ("exp") claim <i>is</i> a default required claim.</p>
+ *
+ * <p>Specifying optional claims will <i>remove</i> any matching claims from the default set of required claims.</p>
+ *
+ * <p>An <i>empty</i> list of accepted audiences alone does <i>not</i> remove the audience ("aud") claim from the default set of required claims; the claim must be explicitly specified as optional.</p>
+ */
+public class DefaultConfigurableJwtValidator implements JwtTokenValidator {
+    private static final List<String> DEFAULT_REQUIRED_CLAIMS = List.of(
+        JWTClaimNames.AUDIENCE,
+        JWTClaimNames.EXPIRATION_TIME,
+        JWTClaimNames.ISSUED_AT,
+        JWTClaimNames.ISSUER,
+        JWTClaimNames.SUBJECT
+    );
+    private static final Set<String> PROHIBITED_CLAIMS = Collections.emptySet();
+    private final JWKSource<SecurityContext> jwkSource;
+    private final ConfigurableJWTProcessor<SecurityContext> jwtProcessor;
+
+    public DefaultConfigurableJwtValidator(String issuer, List<String> acceptedAudiences, JWKSource<SecurityContext> jwkSource) {
+        this(issuer, acceptedAudiences, null, jwkSource);
+    }
+
+    public DefaultConfigurableJwtValidator(String issuer, List<String> acceptedAudiences, List<String> optionalClaims, JWKSource<SecurityContext> jwkSource) {
+        acceptedAudiences = Optional.ofNullable(acceptedAudiences).orElse(List.of());
+        optionalClaims = Optional.ofNullable(optionalClaims).orElse(List.of());
+
+        var requiredClaims = difference(DEFAULT_REQUIRED_CLAIMS, optionalClaims);
+        var exactMatchClaims = new JWTClaimsSet.Builder()
+            .issuer(issuer)
+            .build();
+        var keySelector = new JWSVerificationKeySelector<>(
+            JWSAlgorithm.RS256,
+            jwkSource
+        );
+        var claimsVerifier = new DefaultJwtClaimsVerifier<>(
+            acceptedAudiences(acceptedAudiences, optionalClaims),
+            exactMatchClaims,
+            requiredClaims,
+            PROHIBITED_CLAIMS
+        );
+
+        var jwtProcessor = new DefaultJWTProcessor<>();
+        jwtProcessor.setJWSKeySelector(keySelector);
+        jwtProcessor.setJWTClaimsSetVerifier(claimsVerifier);
+
+        this.jwkSource = jwkSource;
+        this.jwtProcessor = jwtProcessor;
+    }
+
+    @Override
+    public void assertValidToken(String tokenString) throws JwtTokenValidatorException {
+        try {
+            jwtProcessor.process(tokenString, null);
+        } catch (Throwable t) {
+            throw new JwtTokenValidatorException("Token validation failed: " + t.getMessage(), t);
+        }
+    }
+
+    private static Set<String> acceptedAudiences(List<String> acceptedAudiences, List<String> optionalClaims) {
+        if (optionalClaims.contains(JWTClaimNames.AUDIENCE)) {
+            // Make DefaultJWTClaimsVerifier skip the entire audience check (i.e. both match and existence checks)
+            return null;
+        }
+        return new HashSet<>(acceptedAudiences);
+    }
+
+    private static <T> Set<T> difference(List<T> first, List<T> second) {
+        return first.stream()
+            .filter(c -> !second.contains(c))
+            .collect(Collectors.toUnmodifiableSet());
+    }
+
+    protected JWKSource<SecurityContext> getJwkSource() {
+        return this.jwkSource;
+    }
+}
---
+++ token-validation-core/src/main/java/no/nav/security/token/support/core/validation/DefaultJwtClaimsVerifier.java
@@ -0,0 +1,37 @@
+package no.nav.security.token.support.core.validation;
+
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.proc.BadJWTException;
+import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier;
+import com.nimbusds.jwt.util.DateUtils;
+import com.nimbusds.openid.connect.sdk.validators.BadJWTExceptions;
+
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * Extends {@link com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier} with a time check for the issued at ("iat") claim.
+ * The claim is only checked if it exists in the given claim set.
+ */
+public class DefaultJwtClaimsVerifier<C extends SecurityContext> extends DefaultJWTClaimsVerifier<C> {
+
+    public DefaultJwtClaimsVerifier(final Set<String> acceptedAudience,
+                                    final JWTClaimsSet exactMatchClaims,
+                                    final Set<String> requiredClaims,
+                                    final Set<String> prohibitedClaims) {
+        super(acceptedAudience, exactMatchClaims, requiredClaims, prohibitedClaims);
+    }
+
+    public void verify(final JWTClaimsSet claimsSet, final C context) throws BadJWTException {
+        super.verify(claimsSet, context);
+
+        Date iat = claimsSet.getIssueTime();
+        if (iat != null) {
+            Date now = new Date();
+            if (!iat.equals(now) && !DateUtils.isBefore(iat, now, super.getMaxClockSkew())) {
+                throw BadJWTExceptions.IAT_CLAIM_AHEAD_EXCEPTION;
+            }
+        }
+    }
+}
