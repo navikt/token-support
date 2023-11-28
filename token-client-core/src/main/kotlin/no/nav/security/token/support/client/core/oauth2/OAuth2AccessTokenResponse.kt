07ec9a405cf00e79dc7135453c1ac571f4a8a359
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-11-28 13:37:27 +0100

    clean up response

--- token-client-core/src/main/kotlin/no/nav/security/token/support/client/core/oauth2/OAuth2AccessTokenResponse.kt
+++ token-client-core/src/main/kotlin/no/nav/security/token/support/client/core/oauth2/OAuth2AccessTokenResponse.kt
@@ -1,18 +1,9 @@
 package no.nav.security.token.support.client.core.oauth2
 
- class OAuth2AccessTokenResponse @JvmOverloads constructor(var accessToken : String? = null,  var expiresAt : Int? = null,  var expiresIn : Int? = 60, private var additionalParameters : Map<String, Any> = emptyMap()) {
+ data class OAuth2AccessTokenResponse (var access_token : String? = null,  var expires_at : Int? = null,  var expires_in : Int? = 60, private val additionalParameters : Map<String, Any> = emptyMap()) {
 
-
-    //for jackson if it is used for deserialization
-    fun setAccess_token(access_token : String?) {
-        accessToken = access_token
-    }
-
-    fun setExpires_at(expires_at : Int) {
-        expiresAt = expires_at
-    }
-    fun setExpires_in(expires_in : Int) {
-        expiresIn = expires_in
-    }
+     val accessToken = access_token
+     val expiresAt = expires_at
+     val expiresIn = expires_in
 
 }
