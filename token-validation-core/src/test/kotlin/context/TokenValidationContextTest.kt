e1ac9d67eaf8f8f911f349fb381a71a45963598e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-12-07 15:32:47 +0100

    translated a test

---
+++ token-validation-core/src/test/kotlin/context/TokenValidationContextTest.kt
@@ -0,0 +1,29 @@
+package context
+
+import com.nimbusds.jwt.JWTClaimsSet.Builder
+import com.nimbusds.jwt.PlainJWT
+import java.util.concurrent.ConcurrentHashMap
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import no.nav.security.token.support.core.context.TokenValidationContext
+import no.nav.security.token.support.core.jwt.JwtToken
+
+internal class TokenValidationContextTest {
+
+    @Test
+    fun firstValidToken() {
+            val map : MutableMap<String, JwtToken> = ConcurrentHashMap()
+            val tokenValidationContext = TokenValidationContext(map)
+            assertThat(tokenValidationContext.firstValidToken).isEmpty()
+            assertThat(tokenValidationContext.hasValidToken()).isFalse()
+
+            val jwtToken1 = jwtToken("https://one")
+            val jwtToken2 = jwtToken("https://two")
+            map["issuer2"] = jwtToken2
+            map["issuer1"] = jwtToken1
+
+            assertThat(tokenValidationContext.firstValidToken).hasValueSatisfying { jwtToken -> jwtToken.issuer == jwtToken2.issuer }
+        }
+
+    private fun jwtToken(issuer : String) = JwtToken(PlainJWT(Builder().issuer(issuer).subject("subject").build()).serialize())
+}
