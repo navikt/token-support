2c9d44354e9af631eb7b474a634a9dcef4bace3e
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-11-07 10:41:18 +0100

    skip validation for async requests when the client id gone

--- token-validation-filter/src/main/kotlin/no/nav/security/token/support/filter/JwtTokenValidationFilter.kt
+++ token-validation-filter/src/main/kotlin/no/nav/security/token/support/filter/JwtTokenValidationFilter.kt
@@ -1,8 +1,11 @@
 package no.nav.security.token.support.filter
 
+import jakarta.servlet.DispatcherType
+import jakarta.servlet.DispatcherType.*
 import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.FilterConfig
+import jakarta.servlet.RequestDispatcher.ERROR_EXCEPTION
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
@@ -10,14 +13,21 @@ import jakarta.servlet.http.HttpServletResponse
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.core.http.HttpRequest
 import no.nav.security.token.support.core.validation.JwtTokenValidationHandler
+import org.slf4j.LoggerFactory
 
 open class JwtTokenValidationFilter(private val jwtTokenValidationHandler : JwtTokenValidationHandler, private val contextHolder : TokenValidationContextHolder) : Filter {
-
+    private val log = LoggerFactory.getLogger(JwtTokenValidationFilter::class.java)
     override fun destroy() {}
 
     override fun doFilter(request : ServletRequest, response : ServletResponse, chain : FilterChain) {
         if (request is HttpServletRequest) {
-            doTokenValidation(request, response as HttpServletResponse, chain)
+            if (request.dispatcherType == ASYNC && (request.getAttribute(ERROR_EXCEPTION) as? String)?.contains("broken pipe", ignoreCase = true) == true) {
+                log.trace("Skipping token validation for async request, client is gone")
+                chain.doFilter(request, response)
+            }
+            else {
+                doTokenValidation(request, response as HttpServletResponse, chain)
+            }
         }
         else {
             chain.doFilter(request, response)
