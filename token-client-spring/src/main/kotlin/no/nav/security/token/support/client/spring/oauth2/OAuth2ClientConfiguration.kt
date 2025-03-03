78543bcd8a2eca4efa8a27a2b429bd2cc792ae67
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-03 13:29:20 +0100

    bump

--- token-client-spring/src/main/kotlin/no/nav/security/token/support/client/spring/oauth2/OAuth2ClientConfiguration.kt
+++ token-client-spring/src/main/kotlin/no/nav/security/token/support/client/spring/oauth2/OAuth2ClientConfiguration.kt
@@ -9,6 +9,7 @@ import no.nav.security.token.support.client.core.oauth2.OnBehalfOfTokenClient
 import no.nav.security.token.support.client.core.oauth2.TokenExchangeClient
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass
@@ -24,6 +25,7 @@ import org.springframework.web.client.RestClient
 @EnableConfigurationProperties(ClientConfigurationProperties::class)
 @Configuration
 class OAuth2ClientConfiguration : ImportAware {
+    private val log = LoggerFactory.getLogger(OAuth2ClientConfiguration::class.java)
     private var attrs: AnnotationAttributes? = null
     override fun setImportMetadata(meta: AnnotationMetadata) {
         attrs = requireNotNull(fromMap(meta.getAnnotationAttributes(EnableOAuth2Client::class.java.name, false))) { "@EnableOAuth2Client is not present on importing class ${meta.className}" }
@@ -32,6 +34,7 @@ class OAuth2ClientConfiguration : ImportAware {
     @Bean
     fun oAuth2AccessTokenService(bearerTokenResolver: JwtBearerTokenResolver, client: OAuth2HttpClient) =
         if (attrs?.getBoolean("cacheEnabled") == true) {
+            log.trace("Caching is enabled")
             val maxx =  attrs?.getNumber<Long>("cacheMaximumSize") ?: 0
             val skew = attrs?.getNumber<Long>("cacheEvictSkew") ?: 0
             OAuth2AccessTokenService(bearerTokenResolver, OnBehalfOfTokenClient(client), ClientCredentialsTokenClient(client),
